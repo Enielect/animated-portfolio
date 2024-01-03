13e62c599836cdcbccd308d578bc32b618ad9410
eniola <eniolafaithful57@gmail.com>
2024-01-03 15:30:05 +0100

    set up the navbar layout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,17 @@
-import 'navbar.scss'
+import "./navbar.scss";
 
 function Navbar() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="wrapper">
+      <strong className="logo">Eniola</strong>
+      <div className="options">
+        <img src="./facebook.png" alt="facebook" />
+        <img src="./instagram.png" alt="instagram" />
+        <img src="./youtube.png" alt="youtube" />
+        <img src="./dribbble.png" alt="dribble" />
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
