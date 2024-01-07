1da9f8dd09add1b4d11af587f1b77a9198c36711
eniola <eniolafaithful57@gmail.com>
2024-01-07 18:04:20 +0100

    implementing the services page

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,17 @@
+function Services() {
+  return (
+    <div className="services">
+      <div className="title-container">
+        I focus on helping your brand grow and move forward
+        <hr />
+      </div>
+      <div className="middle-container">
+        <img src="" alt="" />
+        <span>Unique Ideas For Your Business.</span>
+        <button>WHAT WE DO?</button>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
