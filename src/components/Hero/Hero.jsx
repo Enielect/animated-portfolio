aa891f3ff9eb1e22ab24de52be9cfcb3bdf3ff56
eniola <eniolafaithful57@gmail.com>
2024-01-08 12:01:46 +0100

    added slidercontent animation to the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,40 @@
-import './hero.scss'
+import { motion } from "framer-motion";
+
+import "./hero.scss";
 
 function Hero() {
+  const sliderVariants = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: "-220%",
+      transition: {
+        repeat: Infinity,
+        duration: 20,
+        repeatType: "mirror",
+      },
+    },
+  };
+
   return (
     <div className="hero">
       <div className="first">
         <span>Harvey Tyler</span>
-        <h1 className='section-content'>Web developer and UI designer</h1>
+        <h1 className="section-content">Web developer and UI designer</h1>
         <div className="buttons">
           <button>See the Latest Works</button>
           <button>Contact Me</button>
         </div>
       </div>
+      <motion.h1
+        className="animated-text"
+        initial="initial"
+        animate="animate"
+        variants={sliderVariants}
+      >
+        FrontEnd Developer
+      </motion.h1>
       <div className="image">
         <img src="./hero.png" alt="" />
       </div>
