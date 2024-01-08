4507b20b546f2512825d678a66de075186580b4f
eniola <eniolafaithful57@gmail.com>
2024-01-08 12:01:12 +0100

    added the parallax page to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./app.scss";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Parallax from "./components/Parallax/Parallax";
+import Services from "./components/Services/Services";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
@@ -11,9 +13,17 @@ const App = () => {
         <Hero />
         <Sidebar />
       </section>
-      <section className="section">services</section>
-      <section className="section">about</section>
-      <section className="section"> form</section>
+      <section className="section services">
+        <Parallax type="services" />
+      </section>
+      <section className="section">
+        <Services />
+        what is your name bro!!!!
+      </section>
+      <section className="section">
+        {" "}
+        <Parallax type="portfolio" />
+      </section>
       <section className="section"> svg</section>
     </div>
   );
