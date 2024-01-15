a08d9e148fa5a1bd96bb6121e6d5e8de0d24ec6f
eniola <eniolafaithful57@gmail.com>
2024-01-15 17:09:58 +0100

    implementing the parallax effect in the website

--- src/components/Parallax/Parallax.jsx
+++ src/components/Parallax/Parallax.jsx
@@ -1,15 +1,42 @@
+import { useRef } from "react";
 import "./parallax.scss";
+import { useScroll, useTransform, motion } from "framer-motion";
 
 function Parallax({ type }) {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start, start", "end, start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
+  const stars = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
-    <div className="parallax" style={{ background:
-        type === "services"
-          ? "linear-gradient(180deg, #111132, #0c0c1d)"
-          : "linear-gradient(180deg, #111132, #505064)",}}>
-        <img src="./stars.png" className="stars" alt="" />
-        <img src="./planets.png" className="planets" alt="" />
-        <img src="./mountains.png" className="mountains" alt="" />
-    </div>
+    <motion.div
+      ref={ref}
+      className="parallax"
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, #111132, #0c0c1d)"
+            : "linear-gradient(180deg, #111132, #505064)",
+      }}
+    >
+      <motion.img
+        style={{ x: stars }}
+        src="./stars.png"
+        className="stars"
+        alt=""
+      />
+      <motion.h1 style={{ y: yText }} className="text">
+        What We Do
+      </motion.h1>
+      <img src="./planets.png" className="planets" alt="" />
+      <img src="./mountains.png" className="mountains" alt="" />
+    </motion.div>
   );
 }
 
