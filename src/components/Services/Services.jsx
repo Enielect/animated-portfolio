273a0da0cd6d3c3181cb79385dde2e3de847200e
eniola <eniolafaithful57@gmail.com>
2024-01-15 17:17:44 +0100

    designed the services page from scratch

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,14 +1,64 @@
+import "./services.scss";
+
 function Services() {
   return (
     <div className="services">
       <div className="title-container">
-        I focus on helping your brand grow and move forward
-        <hr />
+        <span className="text">
+          {" "}
+          I focus on helping your brand grow and move forward
+        </span>
+        <span className="line">
+          <hr />
+        </span>
       </div>
       <div className="middle-container">
-        <img src="" alt="" />
-        <span>Unique Ideas For Your Business.</span>
+        <img src="./people.webp" alt="" />
+        <span className="big-text">Unique Ideas For Your Business.</span>
         <button>WHAT WE DO?</button>
+      </div>
+      <div className="last-section">
+       
+        <div className="card">
+          <h3>Branding</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="card">
+          <h3>Branding</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="card">
+          <h3>Branding</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="card">
+          <h3>Branding</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
+        </div>
       </div>
     </div>
   );
